9f6aa401fa82f0cdcb0f8ec3e0305f4112016270
Joshua Abraham <sinisterpatrician@gmail.com>
2016-05-31 22:50:53 -0400

    second shell code commit

--- shellcodeutil.py
+++ shellcodeutil.py
@@ -3,6 +3,11 @@
 def stack_string(str):
     reverse = str[::-1]
     hex_string = reverse.encode('hex')
-    
+    if len(hex_string) % 8 != 0:
+        print "String literal not able to be aligned by 4-bytes"
+        pass
+    for i in range(0, len(hex_string), 8):
+        print '0x' + hex_string[0+i:8+i]
 
-print stack_string('/sbin///iptables')
+
+stack_string("/sbin///iptables")
